ba44bcd91c597803d0c9fa36dcfacd7336ad95b3
Elzabé Els <elzzels25@gmail.com>
2024-10-23 10:46:46 +0200

    adding sample data for frontend usage

--- app/page.js
+++ app/page.js
@@ -1,4 +1,10 @@
+import { fetchRecipes } from "../lib/dummyData/fetchSampleData";
 export default function Home() {
+
+  // Usage example
+const allRecipes = fetchRecipes();
+console.log(allRecipes);
+
   return (
     <div>
       <main></main>
