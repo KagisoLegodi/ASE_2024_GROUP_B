82f92bb67ec8f1aeba87d93874b66e4237617cf0
NeoPhetoane <phetoaneneo4@gmail.com>
2024-10-24 10:49:40 +0200

    Map function to display recipes.

--- app/page.js
+++ app/page.js
@@ -1,8 +1,26 @@
-export default function Home() {
+
+import RecipeCard from "./components/RecipeCard";
+
+import { fetchRecipes } from "@/lib/api";
+
+export default async function Home() {
+  
+  let recipes = [];
+  try{
+    const data = await fetchRecipes();
+    recipes = data
+
+  }catch(error){
+
+  }
   return (
-    <div>
-      <main><p>123</p></main>
-      <footer></footer>
-    </div>
+    <main>
+      <h1 className="text-2xl font-bold text-center mb-8">Recipes</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        {recipes.map((recipe) => (
+          <RecipeCard key={recipe._id} recipe={recipe} />
+        ))}
+      </div>
+    </main>
   );
-}
+};
