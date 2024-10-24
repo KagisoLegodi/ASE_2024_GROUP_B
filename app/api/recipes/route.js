04b5d064f05442a72be55825e5c764d5d20b778b
NeoPhetoane <phetoaneneo4@gmail.com>
2024-10-24 14:53:10 +0200

    Pull from main :updates made to dependencies, endpoint route changed.

--- app/api/recipes/route.js
+++ app/api/recipes/route.js
@@ -1,4 +1,4 @@
-import clientPromise from "../../lib/mongodb";
+import clientPromise from "../../../lib/mongodb";
 
 /**
  * API route handler for fetching all recipes from the 'recipes' collection in MongoDB.
@@ -9,24 +9,22 @@ import clientPromise from "../../lib/mongodb";
  * @param {Object} res - The response object used to send back the result.
  * @returns {Promise<void>} Sends a JSON response containing the fetched recipes or an error message.
  */
- const fetchRecipes  = async (_req, res) => {
+ export  async function GET (req) {
   try {
     // Await the MongoDB client connection
     const client = await clientPromise;
     const db = client.db('devdb'); // Connect to the 'devdb' database
 
     // Fetch all documents from the 'recipes' collection and convert them to an array
-    const recipes = await db.collection('recipes').find({}).toArray();
+    const recipes = await db.collection('recipes').find({}).limit(20).toArray();
 
     // Send a 200 (OK) response with the fetched recipes in JSON format
-    res.status(200).json(recipes);
+    return new Response(JSON.stringify({ recipes}), { status: 200 });
   } catch (e) {
     // Log the error to the console for debugging
     console.error(e);
 
     // Send a 500 (Internal Server Error) response with an error message
-    res.status(500).json({ error: 'Failed to fetch data' });
+    return new Response(JSON.stringify({error: 'Failed to fetch data'}), { status: 500 });
   }
 };
-
-export default fetchRecipes;
