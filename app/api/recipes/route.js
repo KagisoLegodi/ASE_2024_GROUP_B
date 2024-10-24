1f722e995d86f4b8f36862f32074b69fe101a6f6
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-10-24 14:23:03 +0200

    Add JSDoc comments to Home component and improve recipe fetching logic

--- app/api/recipes/route.js
+++ app/api/recipes/route.js
@@ -9,22 +9,28 @@ import clientPromise from "../../../lib/mongodb";
  * @param {Object} res - The response object used to send back the result.
  * @returns {Promise<void>} Sends a JSON response containing the fetched recipes or an error message.
  */
-export const GET = async (req, res) => {
+export const GET = async (req) => {
   try {
     // Await the MongoDB client connection
     const client = await clientPromise;
     const db = client.db('devdb'); // Connect to the 'devdb' database
 
+    const dataCollection =  db.collection('recipes')
     // Fetch all documents from the 'recipes' collection and convert them to an array
-    const recipes = await db.collection('recipes').find({}).toArray();
+    const recipes = await dataCollection.find({}).limit(20).toArray();
+
+    const totalNumberOfRecipes = await dataCollection.countDocuments({})
+   
+
+    console.log("total", totalNumberOfRecipes)
 
     // Send a 200 (OK) response with the fetched recipes in JSON format
-    res.status(200).json(recipes);
-  } catch (e) {
+    return new Response(JSON.stringify({recipes, totalNumberOfRecipes} ), {status: 200});
+  } catch (error) {
     // Log the error to the console for debugging
-    console.error(e);
+    console.error(error);
 
     // Send a 500 (Internal Server Error) response with an error message
-    res.status(500).json({ error: 'Failed to fetch data' });
+    return new Response(JSON.stringify({ error: 'Failed to fetch data' }), {status: 500});
   }
 };
