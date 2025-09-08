0d13e626c3c66fb34ea2d46b02e8f88c36627f16
Kagiso Legodi <156658455+KagisoLegodi@users.noreply.github.com>
2025-09-08 10:34:26 +0200

    Update route.js

--- app/api/shoppingList/route.js
+++ app/api/shoppingList/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import clientPromise from "@/lib/mongodb"; // adjust the path if needed
+import clientPromise from "lib/mongodb"; // adjust the path if needed
 
 /**
  * Save a new shopping list for a user.
