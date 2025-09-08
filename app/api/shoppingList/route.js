a2ced74e6e717e9138243543c99f5acda49b1f5c
Kagiso Legodi <156658455+KagisoLegodi@users.noreply.github.com>
2025-09-08 10:51:04 +0200

    Update route.js
    fix mongodb location

--- app/api/shoppingList/route.js
+++ app/api/shoppingList/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import clientPromise from "../lib/mongodb.js";
+import clientPromise from "../../../lib/mongodb";
 
 /**
  * Save a new shopping list for a user.
